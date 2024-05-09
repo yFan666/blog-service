948c9d9d5512fd27de322a18b6441cc6241005c4
weiyifan <colawyc@gmail.com>
2024-05-09 20:33:02 +0800

    feat: :memo: Swagger构建

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('youli接口文档')
+    .setDescription('描述')
+    .setVersion('1')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('/api-docs', app, document)
+
   await app.listen(3000);
 };
 bootstrap();
